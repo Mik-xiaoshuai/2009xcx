1ae8b190637652f1ca13782e5830d8140d0feca2
Mik-xiaoshuai <you@example.com>
2021-05-29 17:14:20 +0800

    vue login

--- vue/vue_04/src/main.js
+++ vue/vue_04/src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import router from './router'
-
+import './assets/css/common.css'
 createApp(App).use(ElementPlus).use(router).mount('#app')
